429a6cd0d8b96489c21a5a981d60b1a737b69332
Michael <kalumichael153@gmail.com>
2023-11-22 22:36:11 +0100

    The first part of the python compiler

---
+++ compiler.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass
+from enum import auto, Enum
+from typing import Any, Generator
+
+from Parser import BinOp
+
+class BytecodeType(Enum):
+    BINOP = auto()
+    PUSH = auto()
+
+@dataclass
+class Bytecode:
+    type: BytecodeType
+    value: Any = None
+
+class Compiler:
+    def __init__(self, tree: BinOp) -> None:
+        self.tree = tree
+
+    def compile(self) -> Generator[Bytecode, None, None]:
+        left = self.tree.left
+        yield Bytecode(BytecodeType.PUSH, left.value)
+
+        right = self.tree.right
+        yield Bytecode(BytecodeType.PUSH, right.value)
+
+        yield Bytecode(BytecodeType.BINOP, self.tree.op)
+
+if __name__ == "__main__":
+    from tokenizer import Tokenizer
+    from Parser import Parser
+
+    compiler = Compiler(Parser(list(Tokenizer("3 + 5"))).parse())
+    for bc in compiler.compile():
+        print(bc)
---
+++ tokenizer.py
@@ -0,0 +1,52 @@
+from dataclasses import dataclass
+from enum import Enum, auto
+from typing import Any, Generator
+from string import digits
+
+class TokenType(Enum):
+    INT = auto()
+    PLUS = auto()
+    MINUS = auto()
+    EOF = auto()
+
+@dataclass
+class Token:
+    type: TokenType
+    value: Any = None
+
+class Tokenizer:
+    def __init__(self, code: str)-> None:
+        self.code = code
+        self.ptr: int = 0
+
+    def next_token(self) -> Token:
+        while self.ptr < len(self.code) and self.code[self.ptr] == " ":
+            self.ptr += 1
+        
+        if self.ptr == len(self.code):
+            return Token(TokenType.EOF)
+
+        char = self.code[self.ptr]
+        self.ptr += 1
+        if char == "+":
+            return Token(TokenType.PLUS)
+        elif char == '-':
+            return Token(TokenType.MINUS)
+        elif char in digits:
+            return Token(TokenType.INT, int(char))
+        else:
+            raise RuntimeError(f"Can't tokenize {char!r}.")
+        
+    def __iter__(self) -> Generator[Token, None, None]:
+        while (token := self.next_token()).type != TokenType.EOF:
+            yield token
+        yield token # Yield  thE EOF token too.
+
+
+if __name__ == "__main__":
+    code = "1 + 2 + 3 + 4 - 5 - 6 - 7 - 8"
+    tokenizer = Tokenizer(code)
+    print(code)
+    for tok in tokenizer:
+        print(f"\t{tok.type}, {tok.value}")
+
