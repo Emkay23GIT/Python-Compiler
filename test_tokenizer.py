23608e16449185767e5ad86d8d8f8606c5cd8624
Michael <kalumichael153@gmail.com>
2023-11-24 10:12:56 +0100

    new changes made

--- test_tokenizer.py
+++ test_tokenizer.py
@@ -4,60 +4,60 @@ from tokenizer import Token, Tokenizer, TokenType
 def  test_tokenizer_addition():
     tokens = list(Tokenizer("3 + 5"))
     assert tokens == [
- 3),
+        Token(TokenType.INT, 3),
         Token(TokenType.PLUS),
- 5),
+        Token(TokenType.INT, 5),
         Token(TokenType.EOF),
     ]
 
 def test_tokenizer_subtraction():
     tokens = list(Tokenizer("3 - 6"))
     assert tokens == [
- 3),
+        Token(TokenType.INT, 3),
         Token(TokenType.MINUS),
- 6),
+        Token(TokenType.INT, 6),
         Token(TokenType.EOF),
     ]
 
 def test_tokenizer_additions_and_subtractions():
     tokens = list(Tokenizer("1 + 2 + 3 + 4 - 5 - 6 + 7 - 8"))
     assert tokens == [
- 1),
+        Token(TokenType.INT, 1),
         Token(TokenType.PLUS),
- 2),
+        Token(TokenType.INT, 2),
         Token(TokenType.PLUS),
- 3),
+        Token(TokenType.INT, 3),
         Token(TokenType.PLUS),
- 4),
+        Token(TokenType.INT, 4),
         Token(TokenType.MINUS),
- 5),
+        Token(TokenType.INT, 5),
         Token(TokenType.MINUS),
- 6),
+        Token(TokenType.INT, 6),
         Token(TokenType.PLUS),
- 7),
+        Token(TokenType.INT, 7),
         Token(TokenType.MINUS),
- 8),
+        Token(TokenType.INT, 8),
         Token(TokenType.EOF),
     ]
 
 def test_tokenizer_additions_and_subtractions_with_whitespaces():
     tokens = list(Tokenizer("   1+    2   +3+4-5 -  6 + 7 - 8     "))
     assert tokens == [
- 1),
+        Token(TokenType.INT, 1),
         Token(TokenType.PLUS),
- 2),
+        Token(TokenType.INT, 2),
         Token(TokenType.PLUS),
- 3),
+        Token(TokenType.INT, 3),
         Token(TokenType.PLUS),
- 4),
+        Token(TokenType.INT, 4),
         Token(TokenType.MINUS),
- 5),
+        Token(TokenType.INT, 5),
         Token(TokenType.MINUS),
- 6),
+        Token(TokenType.INT, 6),
         Token(TokenType.PLUS),
- 7),
+        Token(TokenType.INT, 7),
         Token(TokenType.MINUS),
- 8),
+        Token(TokenType.INT, 8),
         Token(TokenType.EOF),
     ]
 
@@ -70,7 +70,7 @@ def test_tokenizer_raises_error_on_garbage():
     [
         ("+", Token(TokenType.PLUS)),
         ("-", Token(TokenType.MINUS)),
-        ("3" 3)),
+        ("3", Token(TokenType.INT, 3)),
     ],
 )
 
@@ -89,4 +89,4 @@ def test_tokenizer_recognises_each_token(code: str, token: Token):
 )
 def test_tokenizer_long_integers(code: str, expected_value: int):
     tokens = list(Tokenizer(code))
-    assert [token.value for token in tokens[:-1]] == expected_value  # Compare only values, excluding EOF
+    assert [token.value for token in tokens[:-1]] == [expected_value], f"Expected {expected_value}, but got {[token.value for token in tokens[:-1]]}" # Compare only values, excluding EOF
